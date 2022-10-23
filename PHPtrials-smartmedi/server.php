17b89b4989ccfff5e5e8c31f42903d01867c73f4
Aisha Rashid <aisha.wrashid64@gmail.com>
2022-10-23 14:30:50 +0300

    removing unnecessary lines of code from server.php

--- PHPtrials-smartmedi/server.php
+++ PHPtrials-smartmedi/server.php
@@ -85,52 +85,6 @@ if (isset($_POST['reg_user'])) {
 	}
 }
 
-// Completing Profile code
-if (isset($_POST['prof_reg'])) {
- 
-	// Receiving the values entered and storing
-	// in the variables
-	// Data sanitization is done to prevent
-	// SQL injections
-	$FirstName = mysqli_real_escape_string($db, $_POST['FirstName']);
-	$LastName = mysqli_real_escape_string($db, $_POST['LastName']);
-	$TelNo = mysqli_real_escape_string($db, $_POST['TelNo']);
-	$IDNo = mysqli_real_escape_string($db, $_POST['IDNo']);
-	$DOB = mysqli_real_escape_string($db, $_POST['DOB']);
-	$gender = mysqli_real_escape_string($db, $_POST['gender']);
-	
-
-	// Ensuring that the user has not left any input field blank
-	// error messages will be displayed for every blank input
-	if (empty($FirstName)) { array_push($errors, "First Name is required"); }
-	if (empty($LastName)) { array_push($errors, "Last Name is required"); }
-	if (empty($TelNo)) { array_push($errors, "Telephone Number is required"); }
-	if (empty($IDNo)) { array_push($errors, "ID Number is required"); }
-	if (empty($DOB)) { array_push($errors, "Date of Birth is required"); }
-	//if (empty($gender)) { array_push($errors, "Password is required"); }
-	
-	// If the form is error free, then register the user
-	if (count($errors) == 0) {
-				
-		$query = "SELECT * FROM patients WHERE username=
-				'$username' AND password='$password'";
-				
-		$results = mysqli_query($db, $query);
-				
-	if (mysqli_num_rows($results) == 1) {
-		// Inserting data into table
-		$query = "INSERT INTO patients (FirstName, LastName, TelNo, IDNo, DOB, gender)
-				VALUES('$username', '$FirstName' , '$LastName' , '$TelNo' , '$IDNo' , '$DOB' , '$gender')";
-		}
-		
-		// Welcome message
-		$_SESSION['success'] = "You have successfully craeted a profile with SmartMedi";
-		
-		// Page on which the user will be
-		// redirected after logging in
-		header('location: dashboard.php');
-	}
-}
 
 // User login
 if (isset($_POST['login_user'])) {
@@ -180,50 +134,4 @@ if (isset($_POST['login_user'])) {
 		}
 	}
 }
-if (isset($_POST[''])) {
-	
-	// Data sanitization to prevent SQL injection
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$password = mysqli_real_escape_string($db, $_POST['password']);
-
-	// Error message if the input field is left blank
-	if (empty($username)) {
-		array_push($errors, "Username is required");
-	}
-	if (empty($password)) {
-		array_push($errors, "Password is required");
-	}
-
-	// Checking for the errors
-	if (count($errors) == 0) {
-		
-		// Password matching
-		$password = md5($password);
-		
-		$query = "SELECT * FROM patients WHERE username=
-				'$username' AND password='$password'";
-		$results = mysqli_query($db, $query);
-
-		// $results = 1 means that one user with the
-		// entered username exists
-		if (mysqli_num_rows($results) == 1) {
-			
-			// Storing username in session variable
-			$_SESSION['username'] = $username;
-			
-			// Welcome message
-			$_SESSION['success'] = "You have logged in!";
-			
-			// Page on which the user is sent
-			// to after logging in
-			header('location: dashboard.php');
-		}
-		else {
-			
-			// If the username and password doesn't match
-			array_push($errors, "Username or password incorrect");
-		}
-	}
-}
-
 ?>
