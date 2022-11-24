3f89665d87b433c15627fbedce8084228e2556be
Aisha Rashid <aisha.wrashid64@gmail.com>
2022-11-24 14:07:05 +0300

    new table added

---
+++ phptrials-smartmedi (1).sql
@@ -0,0 +1,265 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2022 at 12:05 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phptrials-smartmedi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood`
+--
+
+CREATE TABLE `blood` (
+  `id` int(11) NOT NULL,
+  `bloodgroup` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `blood`
+--
+
+INSERT INTO `blood` (`id`, `bloodgroup`) VALUES
+(1, 'A+'),
+(2, 'A-'),
+(3, 'B+'),
+(4, 'B-'),
+(5, 'AB+'),
+(6, 'AB-'),
+(7, 'O+'),
+(8, 'O-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `docspecialty`
+--
+
+CREATE TABLE `docspecialty` (
+  `id` int(11) NOT NULL,
+  `specialty` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `docspecialty`
+--
+
+INSERT INTO `docspecialty` (`id`, `specialty`) VALUES
+(1, 'Critical Care and Emergency'),
+(2, 'Dermatology'),
+(3, 'Diagnostic Radiology'),
+(4, 'Endocrinology and Metabolism'),
+(5, 'Gastroenterology'),
+(6, 'General Surgery'),
+(7, 'General/Clinical Pathology'),
+(8, 'Geriatric Medicine'),
+(9, 'Hematology'),
+(10, 'Medical Microbiology and Infectious Diseases'),
+(11, 'Medical Oncology'),
+(12, 'Nephrology'),
+(13, 'Neurology'),
+(14, 'Neurosurgery'),
+(15, 'Obstetrics/Gynecology'),
+(16, 'Ophthalmology'),
+(17, 'Orthopedic Surgery'),
+(18, 'Otolaryngology'),
+(19, 'Pediatrics'),
+(20, 'Physical Medicine and Rehabilitation (PM & R)'),
+(21, 'Plastic Surgery	'),
+(22, 'Psychiatry'),
+(23, 'Public Health and Preventive Medicine (PhPm)'),
+(24, 'Radiation Oncology'),
+(25, 'Respirology'),
+(26, 'Rheumatology'),
+(27, 'Urology');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `nationalid` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `hospital` varchar(255) NOT NULL,
+  `workid` int(11) NOT NULL,
+  `specialty` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`id`, `nationalid`, `fname`, `lname`, `hospital`, `workid`, `specialty`, `password`) VALUES
+(10910410, 648392384, 'Fatuma', 'Wairimu', 'Gertrude\'s Children\'s Hospital Muthaiga', 637483, 'Nephrology', 'fdc0978bc0cc4c37c3e3d44fc63ee487'),
+(23386746, 263830028, 'Imran', 'Mbulika', 'Brother André Medical Center Dandora', 2474357, 'General/Clinical Pathology', 'e18fdc9fa7cc2b5f4e497d21a48ea3b7'),
+(23510146, 63849286, 'wanja', 'aisha', 'Avenue Hospital', 6283920, 'Neurosurgery', ''),
+(67066255, 638992847, 'Rehema', 'rashid', 'Avenue Hospital', 53748920, 'Diagnostic Radiology', ''),
+(70336988, 2147483647, 'Imran', 'Mbulika', 'Gertrude\'s Children\'s Hospital Muthaiga', 6389403, 'Pediatrics', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospitals`
+--
+
+CREATE TABLE `hospitals` (
+  `hospitalID` int(11) NOT NULL,
+  `hospitalname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hospitals`
+--
+
+INSERT INTO `hospitals` (`hospitalID`, `hospitalname`) VALUES
+(1, 'AAR Hospital'),
+(2, 'Aga Khan University Hospital, Nairobi'),
+(3, 'Avenue Hospital'),
+(4, 'Bristol Park Hospital Tasia Embakasi'),
+(5, 'Brother André Medical Center Dandora'),
+(6, 'Coptic Hospital Nursing Hospital'),
+(7, 'Gertrude\'s Children\'s Hospital Muthaiga'),
+(8, 'Guru Nanak Ramgarhia Sikh Hospital'),
+(9, 'The Karen Hospital'),
+(10, 'Kenyatta National Hospital'),
+(11, 'Kenyatta University Teaching, Referral, and Research Hospital');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `names`
+--
+
+CREATE TABLE `names` (
+  `id` int(11) NOT NULL,
+  `notes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `TelNo` varchar(255) NOT NULL,
+  `IDNo` int(11) NOT NULL,
+  `DOB` date NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `pic` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`ID`, `FirstName`, `LastName`, `username`, `TelNo`, `IDNo`, `DOB`, `gender`, `email`, `password`, `pic`) VALUES
+(20, 'Aisha', 'Rashid', 'Jazzy', '+254703277202', 2147483647, '1990-02-09', 'Female', 'chuchuaisha@gmail.com', 'a381bedb5d4478053eb04be35f8798dd', ''),
+(21, 'Farida', 'Karanja', 'Farida', '8450039344', 2147483647, '2001-06-12', 'Female', 'farida@yahoo.com', '41a6a36598a0acd0d0c3aac95edc7b35', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blood`
+--
+ALTER TABLE `blood`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `docspecialty`
+--
+ALTER TABLE `docspecialty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  ADD PRIMARY KEY (`hospitalID`);
+
+--
+-- Indexes for table `names`
+--
+ALTER TABLE `names`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blood`
+--
+ALTER TABLE `blood`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `docspecialty`
+--
+ALTER TABLE `docspecialty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  MODIFY `hospitalID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `names`
+--
+ALTER TABLE `names`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
